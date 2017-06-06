49d904b8359fd9bcfeeed55394ddde3484ff6df4
bgveenstra <b.g.veenstra@gmail.com>
2017-06-06 12:26:37 -0700

    add GET / route and simple logger middleware

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+// REQUIREMENTS
 var express = require('express');
 var app = express();
 
+
+app.use(logger);
+function logger(req, res, next){
+  console.log(req.method + " " + req.path);
+  next();
+}
+
+// ROUTES
+// app.VERB(PATH, CALLBACK/CONTROLLER)
+// says what to do when we get a request matching VERB PATH
+app.get('/', function(req, res){
+  res.send('Start blogging!');
+})
+
+// LISTEN
 app.listen(3000, function(){
   console.log("app listening!")
 })
